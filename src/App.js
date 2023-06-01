b0bc864a7bb298a23c08eb149962753672cf9eee
Nidoveralis <nidoveralis@gmail.com>
2023-06-01 13:09:10 +0400

    feat: create normalize.css and added in app.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './vendor/normalize.css';
+import { Route, Switch, Redirect, useHistory, useLocation } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
